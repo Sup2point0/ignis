75fcacd1dc0b68e30a0d67e9bc220106bf59774c
Sup#2.0 <sup2point0@gmail.com>
2024-04-09 13:38:41 +0100

    add `link.py`

---
+++ ygo_api/link.py
@@ -0,0 +1,26 @@
+import json
+
+import requests
+
+import suptools as sp
+
+
+def get_cards_data(**kwargs):
+  request = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+  if kwargs:
+    request += "?" + "&".join(f"{key}={val}" for key, val in kwargs.items())
+
+  response = requests.get(request)
+  status = response.status_code
+
+  if status == 200:
+    return response.json()
+  else:
+    raise Exception(f"status {status}")
+
+
+def save_cards_data(data: dict):
+  with open("../assets/data/data.json", "r+") as file:
+    existing = json.load(file)
+    existing.update(data)
+    sp.io.overwrite(file, json.dumps(existing))
