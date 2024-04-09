fb6c7adf4459faf6f9f0049f13d828f1a2c4a614
Sup#2.0 <sup2point0@gmail.com>
2024-04-09 17:33:46 +0100

    add `MonsterCard` class

---
+++ ygo/classes/monster.py
@@ -0,0 +1,15 @@
+from .card import Card
+
+
+class MonsterCard(Card):
+  '''Represents a monster card.'''
+
+  def __init__(self,
+    type: str,
+    attribute: str,
+    *args, **kwargs
+  ):
+    super().__init__(*args, **kwargs)
+
+    self.type = type
+    self.attribute = attribute
