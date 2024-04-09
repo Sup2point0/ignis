cfc5b3aad3a7df806d377e4997439a166a6474de
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-04-09 16:13:52 +0100

    develop `Ai`

--- ignis/ai.py
+++ ignis/ai.py
@@ -12,4 +12,12 @@ class Ai(Ignis):
   '''Ai, the Ignis for predicting Monster Types and Attributes.'''
 
   def __init__(self):
-    super().__init__()
+    super().__init__(
+      root = layers.Input(shape = (100, 100, 3)),  ### FIXME
+      layers = [
+        layers.Conv2D(16, 3, activation = "relu"),
+        layers.MaxPooling2D(2),
+        layers.Conv2D(16, 3, activation = "relu"),
+        layers.MaxPooling2D(2),
+      ],
+    )
