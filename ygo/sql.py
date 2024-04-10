ad77aee884b4f2f850bb971064a5e3c38062b380
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-04-10 19:42:36 +0100

    update SQL query engine

--- ygo/sql.py
+++ ygo/sql.py
@@ -24,3 +24,20 @@ def update_cards_data(cards: list):
   with closing(sqlite.connect(ROUTE)) as ctx:
     with ctx:
       ctx.executemany(query("update-monsters"), (card.as_json() for card in cards))
+
+
+def get_monsters_data(**kwargs):
+  q = '''
+    SELECT id, name, art
+    FROM cards
+    ORDER BY name
+  '''
+
+  if kwargs:
+    q += "WHERE " + "\n".join(f"{key} = {val}" for key, val in kwargs.items())
+  
+  with closing(sqlite.connect(ROUTE)) as ctx:
+    out = ctx.execute()
+    # out = out.fetchall()
+
+  return out.fetchall()
