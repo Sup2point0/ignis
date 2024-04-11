d1a7fb40ea251a8b422667e79c3517cd907508cd
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-04-11 21:00:22 +0100

    update `refresh.sql`

--- ygo/queries/refresh.sql
+++ ygo/queries/refresh.sql
@@ -1,19 +1,17 @@
 -- create a new cards database
 
--- DROP TABLE monsters;
 DROP TABLE cards;
 
 CREATE TABLE cards(
-  id INT(9) PRIMARY KEY,
+  id INT PRIMARY KEY,
   name TEXT NOT NULL,
-  art BYTES,
-  type TEXT NOT NULL
-);
-
-CREATE TABLE monsters(
-  id INT(15) PRIMARY KEY,
-  FOREIGN KEY(id) REFERENCES cards,
-  type TEXT NOT NULL,
-  race TEXT NOT NULL,
-  attribute ENUM('divine', 'light', 'dark', 'fire', 'water', 'earth', 'wind') NOT NULL,
+  art BYTES NOT NULL,
+  type TEXT,
+  kind TEXT,
+  race TEXT,
+  attribute TEXT,
+  lvl INT,
+  atk INT,
+  def INT,
+  pend INT
 );
