ccc3b046100a43ada1d50cbacca5867328f1a675
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-04-19 09:01:15 +0100

    develop `Ignis`

--- ignis/ignis.py
+++ ignis/ignis.py
@@ -2,7 +2,9 @@
 Implements the `Ignis` base class for Ignis to derive from.
 '''
 
-from tensorflow.keras import Model, layers, optimizers
+from tensorflow import keras
+
+import suptools as sup
 
 
 class Ignis:
@@ -12,27 +14,31 @@ class Ignis:
     rate = 10 ** -6
     epochs = 12
 
-  def __init__(self,
-    root,
-    layers: list,
-    rate: float = None,
-    epochs: int = None,
-  ):
+  def __init__(self, shard: str):
     '''Create an Ignis.'''
 
-    self.rate = learn_rate or Ignis.defaults.rate
+    self.shard = shard
+    self.model = None
 
-    self.summon(root, layers)
+    self.path = f"../assets/models/{self.shard}"
 
-  def summon(self, root, layers):
-    '''Initialise the network.'''
+  def summon(self,
+    root: keras.layers.Input,
+    layers: list,
+    epochs: int = None,
+    rate: float = None,
+  ):
+    '''Create a network.'''
     
     network = root
     
     for layer in layers:
       network = layer(network)
 
-    self.model = Model(root, network)
+    self.model = keras.Model(root, network)
+    
+    self.rate = rate or Ignis.defaults.rate
+    self.epochs = epochs or Ignis.defaults.epochs
 
   def activate(self, data):
     '''Train the network.'''
@@ -42,13 +48,29 @@ class Ignis:
 
     self.model.compile(
       loss = "categorical_crossentropy",
-      optimizer = optimizers.Adam(learning_rate = self.rate),
+      optimizer = keras.optimizers.RMSprop(lr = self.rate),
+      metrics = ["acc"],
     )
 
-    self.model.fit_generator(
-      data,
-      epochs = self.epochs
-    )
+    self.model.fit_generator(data, epochs = self.epochs)   ### FIXME
+
+  def save(self):
+    '''Save the model to local files.'''
+
+    if self.model is None:
+      raise ValueError("network nonexistent!")
+
+    with open(self.path + ".json", "w") as file:
+      sup.io.overwrite(file, self.mode.to_json())
+
+    self.model.save_weights(self.path + ".h5")
+
+  def draw(self):
+    '''Load the model from local files.'''
+
+    with open(self.path + ".json", "r") as file:
+      self.model = keras.models.model_from_json(file.read())
+      self.model.load_weights(self.path + ".h5")
 
   def declare(self, data):
     '''Use the network on given data.'''
