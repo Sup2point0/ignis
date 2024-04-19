f91a5e98c230d2c84dd4dfd23f3a195be5e9f823
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-04-19 09:08:41 +0100

    develop `Ai`

--- ignis/ai.py
+++ ignis/ai.py
@@ -2,22 +2,24 @@
 Ai, the Ignis for predicting Monster Types and Attributes.
 '''
 
-from tensorflow.keras import Model, layers
+from tensorflow.keras import layers
 
-from .ignis import Ignis
 import ygo
+from .ignis import Ignis
 
 
 class Ai(Ignis):
   '''Ai, the Ignis for predicting Monster Types and Attributes.'''
 
-  def __init__(self):
-    super().__init__(
-      root = layers.Input(shape = (100, 100, 3)),  ### FIXME
+  def summon(self):
+    super().summon(
+      root = layers.Input(shape = (624, 624, 3)),
       layers = [
         layers.Conv2D(16, 3, activation = "relu"),
         layers.MaxPooling2D(2),
         layers.Conv2D(16, 3, activation = "relu"),
         layers.MaxPooling2D(2),
+        layers.Flatten(),
+        layers.Dense(1, activation = "softmax"),
       ],
     )
