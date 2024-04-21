9c8989785e380eef5d42f446679c4a2e10bde482
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-04-21 10:51:35 +0100

    develop `bot.py`

--- discord/bot.py
+++ discord/bot.py
@@ -1,5 +1,5 @@
 '''
-Ai, the Discord bot.
+Ai.
 '''
 
 import os
@@ -10,7 +10,9 @@ from nextcord import ui
 from nextcord.ui import View, Modal, button
 from nextcord.ext import commands
 
+import suptools as sup
 from ignis import Ai
+from .load import load
 
 
 bot = commands.Bot(intents = disc.Intents.all())
@@ -22,5 +24,11 @@ async def test(interaction):
   await interaction.response.send("testing!", ephemeral = True)
 
 
-if __name__ == "__main__":
+def script():
+  bot.add_cog(load(bot))
+  
   bot.run(os.getenv("LINK"))
+
+
+if __name__ == "__main__":
+  sup.run(script, vitals = True)
