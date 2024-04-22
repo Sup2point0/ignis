56633537699bbe49e0aef16f07218cd260831280
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-04-22 19:15:58 +0100

    add `disc/play.py`

---
+++ disc/play.py
@@ -0,0 +1,64 @@
+'''
+Implements the `play` family of slash commands.
+'''
+
+import random
+
+import nextcord as disc
+from nextcord import Embed, SlashOption, SelectOption
+from nextcord import ui, ButtonStyle
+from nextcord.ui import Modal, button
+from nextcord.ext import commands
+
+import ygo
+
+
+class play(commands.Cog):
+  '''Commands involving playing games.'''
+
+  def __init__(self, bot):
+    self.bot = bot
+
+    self.games = {
+      "ygordle-word": None,
+      "ygordle-card": None,
+    }
+    '''Games in progress.'''
+
+  @ disc.slash_command()
+  async def play(self, ctx):
+    pass
+
+  @ play.subcommand()
+  async def ygordle(self, ctx):
+    pass
+  
+  ## /play ygordle word ##
+  @ ygordle.subcommand()
+  async def word(self, ctx,
+    length: int = SlashOption(
+      "characters", "the number of characters the word will contain (defaults to 5)",
+      required = False, default = 5,
+    ),
+  ):
+    '''play a game of ygordle where you guess a word'''
+
+    if self.games["ygordle-word"]:
+      await ctx.send("Sorry, game in progress!", ephemeral = True)
+      return
+
+    root = await ctx.send(embed = Embed(
+      ...
+    ))
+    thread = await root.create_thread(name = "YGORDLE (word)", auto_archive_duration = 60)
+
+    join = await thread.send(ctx.user.mention)
+    await join.delete()
+
+  class WordVisual(ui.View):
+    def __init__(self):
+      super().__init__(timeout = None)
+
+    @ button(label = "Cancel", style = ButtonStyle.danger, custom_id = "ygordle.word.cancel")
+    async def cancel(self, button, ctx):
+      pass
