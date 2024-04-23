48b7b239b2118c9bee9a871b0d9cec000ff7f73a
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-04-23 10:03:35 +0100

    update `ygo.__init__.py`

--- ygo/__init__.py
+++ ygo/__init__.py
@@ -2,6 +2,8 @@
 Handles interactions with the YGOPRODECK API to supply cards data for the ignis.
 '''
 
-from . import api, link, sql
-
+from . import api
+from . import sql
+from . import link
+from . import deck
 from .classes import *
