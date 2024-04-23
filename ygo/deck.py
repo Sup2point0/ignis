ef50f736240fd16c0fcf576e1718a3ca85829286
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-04-23 10:00:20 +0100

    add `ygo.deck` for interacting with XML deck files

---
+++ ygo/deck.py
@@ -0,0 +1,21 @@
+import xml.etree.ElementTree as et
+from collections import Counter
+
+
+def load_deck(data: str) -> dict:
+  '''Load the names of the cards in a deck from an XML file.'''
+
+  out = {}
+
+  root = et.fromstring(data)
+  out["deck"] = root.attrib["name"]
+
+  for deck in root:
+    cards = Counter(card.text for card in deck)
+    out[deck.tag] = {
+      "cards": dict(cards),
+      "cards-total": cards.total(),
+      "cards-unique": len(list(cards)),  ### NOTE can we find a more efficient method?
+    }
+
+  return out
