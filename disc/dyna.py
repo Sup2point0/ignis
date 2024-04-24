66d70f205e6b0b01b3db73a36a8cf897ebff492c
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-04-24 09:51:40 +0100

    cleanup `dyna.py`

--- disc/dyna.py
+++ disc/dyna.py
@@ -9,43 +9,43 @@ FrozenWeightedList = importlib.import_module("weighted-list.Python.frozen_weight
 
 
 punct = FrozenWeightedList(
-  (".", 12),
-  ("!", 10),
-  ("?", 4),
-  ("...", 2),
-  ("!!", 1),
+  (12, "."),
+  (10, "!"),
+  (4, "?"),
+  (2, "..."),
+  (1, "!!"),
 )
 
 
 errors = FrozenWeightedList(
-  3000, Something went wrong
-  3000, An error occurred
-  3000, An exception occurred
-  2500, An error was encountered
-  2500, An exception was encountered
-  2000, Couldn’t do that
-  1000, Nope couldn’t do that
-  2000, Something failed
-  1000, Something failed ¯\_(ツ)_/¯
-  2000, Something broke
-  1000, Something broke ¯\_(ツ)_/¯
-  500, Something broke, lol "
-  1000, You’re out of luck, I’m afraid – something went wrong
-  1000, The code has, unfortunately, thrown an error
-  1000, Despite our best efforts, the code has broken
-  500, On guard! An exception has occurred.
-  500, I’m not feeling so good...
-  500, I’m broken again, dude...
-  500, You’re killing me here...
-  200, Alright, debugging time.
-  200, Uhhh...
-  200, Yo, a little help here @SHARD-bot?
-  100, Alright, debugging time...
-  100, KOGEKI– I mean, 
-  100, Nandato?
-  50, NANI?
-  50, If you keep at this, I might get Copilot to start writing these error messages for me.
-  50, The fated event has happened...
-  50, RED ALERT RED ALERT ERROR ERROR ERROR "
-  10, @Sup#2.0 my guy come fix me, I’ve broken again 0.o
+  (3000, "Something went wrong"),
+  (3000, "An error occurred"),
+  (3000, "An exception occurred"),
+  (2500, "An error was encountered"),
+  (2500, "An exception was encountered"),
+  (2000, "Couldn’t do that"),
+  (1000, "Nope couldn’t do that"),
+  (2000, "Something failed"),
+  (1000, "Something failed ¯\_(ツ)_/¯"),
+  (2000, "Something broke"),
+  (1000, "Something broke ¯\_(ツ)_/¯"),
+  (500, "Something broke, lol "),
+  (1000, "You’re out of luck, I’m afraid – something went wrong"),
+  (1000, "The code has, unfortunately, thrown an error"),
+  (1000, "Despite our best efforts, the code has broken"),
+  (500, "On guard! An exception has occurred."),
+  (500, "I’m not feeling so good..."),
+  (500, "I’m broken again, dude..."),
+  (500, "You’re killing me here..."),
+  (200, "Alright, debugging time."),
+  (200, "Uhhh..."),
+  (200, "Yo, a little help here @SHARD-bot?"),
+  (100, "Alright, debugging time..."),
+  (100, "KOGEKI– I mean, "),
+  (100, "Nandato?"),
+  (50, "NANI?"),
+  (50, "If you keep at this, I might get Copilot to start writing these error messages for me."),
+  (50, "The fated event has happened..."),
+  (50, "RED ALERT RED ALERT ERROR ERROR ERROR "),
+  (10, "@Sup#2.0 my guy come fix me, I’ve broken again 0.o"),
 )
