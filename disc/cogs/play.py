5a6ee86d64596793fc5eba3799edabcc84887408
Sup#2.0 <sup2point0@gmail.com>
2024-05-02 17:20:16 +0100

    develop `/play ygordle word`

--- disc/cogs/play.py
+++ disc/cogs/play.py
@@ -4,6 +4,8 @@ Implements the `play` family of slash commands.
 
 from __future__ import annotations
 
+import asyncio
+import pathlib
 import random
 
 import nextcord as disc
@@ -15,10 +17,13 @@ from nextcord.ext import commands
 import ygo
 from .. import dyna
 from ..resources import colours
+from .error import ExceptionResponse
 
 
 class Play(commands.Cog):
   '''Commands involving playing games.'''
+
+  ROUTE = pathlib.Path(__file__).absolute().parents[2].joinpath("assets/data")
 
   def __init__(self, bot):
     self.bot = bot
@@ -29,24 +34,26 @@ class Play(commands.Cog):
     }
     '''Games in progress.'''
 
-  ## utility ##
+
+  ## utils ##
   class GameEmbed(Embed):
     '''An embed sent as the root message where game threads are created.'''
     
-    def __init__(game: str, *, player: disc.User):
+    def __init__(self, game: str, *, player: disc.User):
       super().__init__()
       
       self.title = game
       self.description = ""
-      self.colour = colours[game]
+      self.colour = colours.games[game.casefold()]
       self.set_footer(text = "Initialising...")
       self.set_author(name = player.display_name, icon_url = player.avatar.url)
 
-    def set_desc(self, content: str) -> GameEmbed:
+    def set_desc(self, content: str) -> Play.GameEmbed:
       '''Set description of embed and return the modified instance.'''
 
       self.description = content
       return self
+
 
   @ disc.slash_command()
   async def play(self, ctx):
@@ -56,39 +63,106 @@ class Play(commands.Cog):
   async def ygordle(self, ctx):
     pass
   
+
   ## /play ygordle word ##
   @ ygordle.subcommand()
   async def word(self, ctx,
-    # characters: int = SlashOption(
+    # chars: int = SlashOption(
     #   "characters", "the number of characters the word will contain (defaults to 5)",
     #   required = False, default = 5,
     # ),
+    tries: int = SlashOption(
+      "attempts", "the number of tries you have to guess the word",
+      required = False, default = 6,
+    ),
   ):
     '''play a game of ygordle where you guess a word'''
 
-    if isinstance(interaction, disc.Thread):
+    if isinstance(ctx.channel, disc.Thread):
       await ctx.send(dyna.punctuate("Can’t play a game in a thread"), ephemeral = True)
       return
-    if isinstance(interaction, disc.DMChannel):
+    if isinstance(ctx.channel, disc.DMChannel):
       await ctx.send(dyna.punctuate("Can’t play a game in direct messages"))
       return
     if self.games["ygordle-word"]:
       await ctx.send(dyna.punctuate("Sorry, game in progress"), ephemeral = True)
       return
 
-    game = Play.WordGame()
+    game = Play.WordGame(tries = tries)#, chars = chars)
     self.games["ygordle-word"] = game
 
     embed = Play.GameEmbed("YGORDLE (word)", player = ctx.user)
     view = Play.WordView(game)
     root = await ctx.send(embed = embed, view = view)
+
+    game.word = self.pick_ygordle_word("ygordle-words-5L")
     
     thread = await root.create_thread(name = "YGORDLE (word)", auto_archive_duration = 60)
     join = await thread.send(ctx.user.mention)
     await join.delete()
 
     await root.edit(embed = embed.set_footer(text = "Ongoing"))
-    await self.run_ygordle_word(game, thread)
+    await self.run_ygordle_word(ctx, game, thread)
+
+    await root.edit(embed = embed.set_footer(text = "Finished"))
+
+  def pick_ygordle_word(self, source: str):
+    '''Pick a random word from a given word list name.'''
+
+    path = Play.ROUTE.joinpath(f"{source}.txt")
+
+    with open(path) as file:
+      return random.choice([line.strip() for line in file])
+
+  async def run_ygordle_word(self, ctx, game: Play.WordGame, thread: disc.Thread):
+    '''Run the game loop of word YGORDLE.'''
+
+    while game.live:
+      try:
+        guess = await self.wait_for_guess(ctx, "message", thread)
+
+        if guess is None:
+          game.live = False
+          break
+
+        content = guess.content.strip()
+        if len(content) < game.chars:
+          raise ExceptionResponse(desc = f"Your guess needs to be at least {game.chars} letters.")
+        
+        guess = content.casefold().split()[0]
+        letters = list(guess)
+        if len(letters) > game.chars:
+          raise ExceptionResponse(desc = f"Your guess cannot be more than {game.chars} letters.")
+        
+        accuracy = self.evaluate_guess(game, letters)
+        
+        game.guesses.append(guess)
+
+      except ExceptionResponse as e:
+        await thread.send(dyna.punctuate(e.desc))
+
+  async def wait_for_guess(self, ctx, type, channel, timeout = 180):
+    '''Wait for a guess in a thread.'''
+
+    try:
+      return await ctx.bot.wait_for(type, timeout = timeout,
+        check = (lambda message: message.channel == channel and message.author == ctx.author)
+      )
+    except asyncio.TimeoutError:
+      return None
+    
+  def evaluate_guess(self, game: Play.WordGame, guess: list[str]) -> dict[str, str]:
+    '''Evaluate the correctness of the letters in a guess.'''
+
+    # dicts preserve order, so this is fine
+    return {
+      letter:
+        "precise" if game.word[i] == letter else
+        "accurate" if letter in game.word else
+        None
+      for i, letter in enumerate(guess)
+    }
+
 
   class WordView(ui.View):
     '''The view for a word YGORDLE root message.'''
@@ -104,17 +178,12 @@ class Play(commands.Cog):
   class WordGame:
     '''A word YGORDLE game instance.'''
     
-    def __init__(self, chars: int = 5):
+    def __init__(self, tries = 6, chars = 5):
+      self.tries = tries
       self.chars = chars
       
       self.live: bool = True
-      self.done: bool = False
       self.guesses: list[tuple[str]] = []
-
-  async def run_ygordle_word(self, game: Play.WordGame, thread: disc.Thread):
-    '''Run the game loop of word YGORDLE.'''
-
-    while not game.done:
       
 
   ## /play ygordle card ##
