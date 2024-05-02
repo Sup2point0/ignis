953524b8abfafd22eaaca5b96b894dc3ed574bab
Sup#2.0 <sup2point0@gmail.com>
2024-05-02 15:24:46 +0100

    add `disc/resources.py`

---
+++ disc/resources.py
@@ -0,0 +1,22 @@
+'''
+Static resources for utility.
+'''
+
+
+class colours:
+  '''supcode Nova colours.'''
+
+  class blue:
+    nova = 0x4090f1
+  
+  class pink:
+    elec = 0xff0090
+
+  class purp:
+    nova = 0x9040f1
+
+  games = {
+    "ygordle (word)": pink.elec,
+    "ygordle (card)": purp.nova,
+  }
+  '''Accent colours for games.'''
