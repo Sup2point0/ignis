ea6d1a56358f506dec45f1f5de0c89385a1d4121
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-05-17 07:49:51 +0100

    develop `DataSource`

--- ignis/datasource.py
+++ ignis/datasource.py
@@ -8,67 +8,71 @@ from typing import Iterable
 import numpy as np
 from tensorflow import keras
 import keras.utils
+from skimage.io import imread
+from skimage.transform import resize
 
 import suptools as sup
 import ygo
-from ygo.classes import Card, CardArt
+from ygo import Card, CardArt
 
 
 class DataSource(keras.utils.Sequence):
-  '''Sources cards data from the database.'''
+  '''Sources data from the database for training and testing the Ignis.'''
+
+  SOURCE = "../.assets/images/"
 
   def __init__(self,
-    data: list,  ### TODO FIXME
-    art_ids: Iterable[int],
-    labels,
+    data: list[tuple[CardArt, Card]],
+    feature: str,
+    classes: int,
     batchsize = 32,
-    dimensions = (32, 32, 32),
-    channels = 1,
-    classes = 10,
+    resize = (300, 300),
     shuffle = True,
   ):
-    sup.init(self,
-      art_ids = art_ids,
-      labels = labels,
-      batchsize = batchsize,
-      dimensions = dimensions,
-      channels = channels,
-      classes = classes,
-      shuffle = shuffle,
-    )
-    
-    self.indexes = np.arange(len(art_ids))
+    self.data = data
+    self.feature = feature
+    self.classes = classes
+
+    self.points = len(data)
+    self.indexes = np.arange(self.points)
+    self.batchsize = batchsize
+    self.batches = math.floor(self.points / batchsize)
+
+    self.resize = resize
+    self.shuffle = shuffle
 
   def __len__(self):
-    '''Find the number of batches per epoch.'''
-    
-    return math.floor(len(self.art_ids) / self.batchsize)
+    return self.batches
 
   def __getitem__(self, index):
     '''Generate a single batch of data.'''
   
     start = index * self.batchsize
-    stop = (index+1) * self.batchsize
+    stop = min(start + self.batchsize, self.points)
     indexes = self.indexes[start:stop]
 
-    art_ids = [self.art_ids[i] for i in indexes]
-    x, y = self.__data_generation(art_ids)
+    # x data
+    arts = [
+      resize(
+        imread(f"{self.ROUTE}{row[0].art_id}.jpg"),
+        self.resize
+      )
+      for row in self.data[start:stop]
+      # for idx in indexes
+      # for row in self.data[idx]
+    ])
 
-    return x, y
+    # y data
+    features = np.array([
+      getattr(row[1], self.feature)
+      for row in self.data[start:stop]
+    ])
+    labels = keras.utils.to_categorical(features, num_classes = self.classes)
+
+    return arts, labels
 
   def on_epoch_end(self):
     '''Update indices after epochs.'''
     
     if self.shuffle:
-        np.random.shuffle(self.indexes)
-
-  def __data_generation(self, art_ids: Iterable[int]):
-    # x: (n_samples, *dim, channels)
-    images = np.empty((self.batchsize, *self.dim, self.channels))
-    labels = np.empty((self.batchsize), dtype = int)
-
-    for i, art_id in enumerate(art_ids):
-        images[i,] = 0  ### TODO FIXME
-        labels[i] = self.labels[art_id]
-
-    return images, keras.utils.to_categorical(labels, num_classes = self.classes)
+      np.random.shuffle(self.indexes)
