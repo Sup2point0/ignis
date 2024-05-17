59def17ea8e337dfaf915f7ddb3bf852f217535a
Sup#2.0 <sup2point0@gmail.com>
2024-05-17 21:08:09 +0100

    add script for counting features

--- scripts/count-feature-classes.py
+++ scripts/count-feature-classes.py
@@ -4,11 +4,19 @@ import suptools as sup
 def script():
   '''Count the number of distinct classes for a particular card feature.'''
 
+  import json
+
+  import config
   import ygo
 
   data = ygo.sql.load(ygo.MonsterCard)
+  features = set(card.race for card in data)
 
-  sup.log(race = len(set(card.race for card in data)))
+  with open(config.ROOT / "assets/data/ignis-classes.json", "r+") as file:
+    data = json.load(file)
+    data["race"] = {feature: i for i, feature in enumerate(features)}
+    file.seek(0)
+    json.dump(data, file, indent = 2)
 
 
 if __name__ == "__main__":
